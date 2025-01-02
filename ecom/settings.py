96e9685e0c5472aa24d5d421a059ef23d3d54915
SMAman63 <aman15362177@gmail.com>
2025-01-03 00:05:48 +0530

    added allowed hosts for railway

--- ecom/settings.py
+++ ecom/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = 'django-insecure-%be&3#+bp5a1sigo*q19==)^+nks&jg3+*%nuiap_pn_7r-#9#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['amanzon-production.up.railway.app','https://amanzon-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['amanzon-production.up.railway.app','https://amanzon-production.up.railway.app']
 
 # Application definition
 
